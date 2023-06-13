1cae86ab1f9b0de905708c802ea083ac3ada67ce
Giedre <giedre.klima@gmail.com>
2023-06-14 01:21:01 +0300

    element drag n drop

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,62 @@ function App() {
   const containerRef = useRef<HTMLDivElement>(null);
   const boxRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
+  const isClicked = useRef<boolean>(false);
 
+  const coords = useRef<{
+    startX: number,
+    startY: number,
+    lastX: number,
+    lastY: number,
+  }>({
+    startX: 0,
+    startY: 0,
+    lastX: 0,
+    lastY: 0
+  });
+
+  useEffect(() => {
+    if (!boxRef.current || !containerRef.current) return;
+
+    const box = boxRef.current;
+    const container = containerRef.current;
+
+
+    const onMouseDown = (e: MouseEvent) => {
+      isClicked.current = true;
+      coords.current.startX = e.clientX;
+      coords.current.startY = e.clientY;
+    }
+
+    const onMouseUp = (e: MouseEvent) => {
+      isClicked.current = false;
+      coords.current.lastX = box.offsetWidth;
+      coords.current.lastY = box.offsetHeight;
+    }
+
+    const onMouseMove = (e: MouseEvent) => {
+      if (!isClicked.current) return;
+
+      const nextX = e.clientX - coords.current.startX + coords.current.lastX;
+      const nextY = e.clientY - coords.current.startY + coords.current.lastY;
+
+      box.style.top = `${nextY}px`;
+      box.style.left = `${nextX}px`;
+
+    }
+
+    box.addEventListener('mousedown', onMouseDown);
+    box.addEventListener("mouseup", onMouseUp);
+    container.addEventListener("mousemove", onMouseMove);
+    container.addEventListener('mouseleave', onMouseUp);
+
+    const cleanup = () => {
+      box.removeEventListener('mousedown', onMouseDown);
+      box.removeEventListener('mouseup', onMouseUp);
+      container.removeEventListener('mousemove', onMouseMove);
+      container.removeEventListener('mouseleave', onMouseUp);
+    };
+    return cleanup;
   }, []);
 
   return (
